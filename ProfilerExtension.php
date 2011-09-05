bec3c8fb8b8401f5865c0829e9e7f915383d6b4f
Rafał Filipek <rafal.filipek@gmail.com>
2011-09-06 00:19:53 +0200

    Remove stupid code :)

--- ProfilerExtension.php
+++ ProfilerExtension.php
@@ -10,10 +10,6 @@ class ProfilerExtension implements ExtensionInterface {
     
     public function register(Application $app)
     {
-        $app->get('/foo', function() use($app){
-            return $app['twig']->render('__profiler-panel.twig');
-        });
-
         $app['twig.loader']->addPath(__DIR__.'/Resources/views/');
         $app['twig']->addExtension(new TwigProfileExtension($app['twig']));
 
